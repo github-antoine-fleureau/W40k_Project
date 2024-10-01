66310f612d271f8dd2e29f7b6e9e5b4d3e55636b
Fleureau Antoine <104366900+github-antoine-fleureau@users.noreply.github.com>
2024-10-01 14:45:19 +0200

    Update W40k.session.sql

--- W40k.session.sql
+++ W40k.session.sql
@@ -1,11 +1,10 @@
 -- Connect to your database (if your SQL tool allows inline connection strings)
 -- Example for SQL Server
-:connect persow40k.database.windows.net,1433 -U antoinefleureau -P Olic@hay87
+: connect persow40k.database.windows.net,1433 -U antoinefleureau -P Olic@hay87
 
--- If connecting to an Azure SQL Database, the connection might be implicit, or you'd manage this in VSCode or a connection UI.
-
--- Use the target database
+-- Switch to the target database
 USE [W40k];
+GO
 
 
 -- Add more SQL commands here based on your project needs
