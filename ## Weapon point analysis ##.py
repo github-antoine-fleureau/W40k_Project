24e1ecd77b6a4dd046a582959ec4ca105480ad81
antoine fleureau <antoinefleureau@gmail.com>
2025-03-24 16:54:20 +0100

    2nd

--- ## Weapon point analysis ##.py
+++ ## Weapon point analysis ##.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import psycopg2
+import pg8000
 import GPy
 import numpy as np
 
